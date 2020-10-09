0227deb2614bb75d34cb97f23465df4145f3bfd1
MS <hercules11031998@gmail.com>
2020-10-09 21:34:48 +0300

    add ejs

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 3000;
+app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"));
 
+
+
+const port = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('server on'))
